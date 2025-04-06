f0df51678016bc34fedcb342e0bddcadb8d79c17
Mathis Favier <favier.mathis@gmail.com>
2025-04-06 17:53:40 +0200

    udp listener

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,13 @@
 const std = @import("std");
-const net = std.net;
-pub fn main() !void {}
+
+pub fn main() !void {
+    const sock = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, std.posix.IPPROTO.UDP);
+    defer std.posix.close(sock);
+
+    const addr = try std.net.Address.parseIp("127.0.0.1", 55555);
+    try std.posix.bind(sock, &addr.any, addr.getOsSockLen());
+
+    var buf: [1024]u8 = undefined;
+    const len = try std.posix.recvfrom(sock, &buf, 0, null, null);
+    std.debug.print("recv: {s}\n", .{buf[0..len]});
+}
